a3ec80754fbf941788ee53a4303f7c2d4cff0359
nivaj <nivajasingh359@gmail.com>
2022-07-11 23:28:33 +0545

    NMS => Bottom Navigation Configured

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
 
-import 'package:get/get.dart';
-
-import '../controllers/home_controller.dart';
-
-class HomeView extends GetView<HomeController> {
+class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int currentIndex = 0;
+  @override
+  void initState() {
+    currentIndex =0;
+    // TODO: implement initState
+    super.initState();
+  }
+
+
+
+  static const List<Widget> widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+
+    ),
+    Text(
+      'Index 1: Business',
+
+    ),
+    Text(
+      'Index 3: School',
+
+    ),
+    Text(
+      'Index 4: School',
+
+    ),
+    Text(
+      'Index 5: School',
+
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,12 +47,44 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body:  IndexedStack(
+        index: currentIndex,
+        children: widgetOptions,
       ),
+      bottomNavigationBar: BottomNavigationBar(
+       // selectedIconTheme:IconThemeData(color: Colors.grey),
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.shifting,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.blue,
+        onTap: (index) => setState(()=>currentIndex = index),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Customer'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.money),
+              label: 'Payment'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag),
+              label: 'Sales'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.menu_book_rounded),
+              label: 'Ledger'
+          )
+        ],
+
+      ),
+
     );
   }
 }
+
+
