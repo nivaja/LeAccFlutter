1eb98ad255258d08d2b4ac732d3a75ec2abb390d
nivaj <nivajasingh359@gmail.com>
2022-07-12 00:00:53 +0545

    NMS => Individual Module Setup

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:leacc_pos/app/modules/customer/views/customer_view.dart';
+import 'package:leacc_pos/app/modules/ledger/views/ledger_view.dart';
+import 'package:leacc_pos/app/modules/payment/views/payment_view.dart';
+import 'package:leacc_pos/app/modules/sales_order/views/sales_order_view.dart';
+
+import '../../sales/views/sales_view.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -9,82 +15,42 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   int currentIndex = 0;
-  @override
-  void initState() {
-    currentIndex =0;
-    // TODO: implement initState
-    super.initState();
-  }
-
-
-
   static const List<Widget> widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-
-    ),
-    Text(
-      'Index 1: Business',
-
-    ),
-    Text(
-      'Index 3: School',
-
-    ),
-    Text(
-      'Index 4: School',
-
-    ),
-    Text(
-      'Index 5: School',
-
-    ),
+    CustomerView(),
+    SalesOrderView(),
+    PaymentView(),
+   // HomeView(),
+    SalesView(),
+    LedgerView()
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body:  IndexedStack(
+
+      body: IndexedStack(
         index: currentIndex,
         children: widgetOptions,
       ),
       bottomNavigationBar: BottomNavigationBar(
-       // selectedIconTheme:IconThemeData(color: Colors.grey),
+        // selectedIconTheme:IconThemeData(color: Colors.grey),
         unselectedItemColor: Colors.grey,
         type: BottomNavigationBarType.shifting,
         currentIndex: currentIndex,
         selectedItemColor: Colors.blue,
-        onTap: (index) => setState(()=>currentIndex = index),
+        onTap: (index) => setState(() => currentIndex = index),
         items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Customer'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Customer'
-          ),
+              icon: Icon(Icons.edit_calendar), label: 'Sales Order'),
+          BottomNavigationBarItem(icon: Icon(Icons.money), label: 'Payment'),
+       //   BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.money),
-              label: 'Payment'
-          ),
+              icon: Icon(Icons.shopping_bag), label: 'Sales'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag),
-              label: 'Sales'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.menu_book_rounded),
-              label: 'Ledger'
-          )
+              icon: Icon(Icons.menu_book_rounded), label: 'Ledger')
         ],
-
       ),
-
     );
   }
 }
-
-
