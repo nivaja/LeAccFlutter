dba1f4429c835710189027a69c0ea86c9d5bc1fd
nivaj <nivajasingh359@gmail.com>
2022-07-22 09:26:23 +0545

    NMS => Added Form Validation

--- lib/app/modules/common/util/get_value.dart
+++ lib/app/modules/common/util/get_value.dart
@@ -1,11 +1,17 @@
+
 import 'package:dio/dio.dart';
 import 'package:leacc_pos/app/modules/http/dio.dart';
 
 class FrappeGet {
-  static Future<String> value(String docType) async {
-    var queryParams = {"doctype": docType, "fieldname": "name"};
-    Response? response = await DioClient()
+  static Future<String> value({required String docType, String fieldName="name", Map<String,dynamic>? filters}) async {
+    Map<String,dynamic> queryParams;
+    if (filters==null){
+      queryParams = {"doctype": docType, "fieldname": fieldName};
+    }else{
+      queryParams ={"doctype": docType, "fieldname": fieldName,"filters":filters};
+    }
+        Response? response = await DioClient()
         .get('/method/frappe.client.get_value', queryParameters: queryParams);
-    return response?.data['message']['name'];
+    return response?.data['message'][fieldName];
   }
 }
--- lib/app/modules/sales/providers/sales_provider.dart
+++ lib/app/modules/sales/providers/sales_provider.dart
@@ -1,21 +1,29 @@
 
+
 import 'package:dio/dio.dart';
 import 'package:leacc_pos/app/modules/http/dio.dart';
 import 'package:leacc_pos/app/modules/sales/model/sales_item_model.dart';
+import 'package:leacc_pos/app/modules/sales/model/sales_model.dart';
 
 import '../../common/util/get_value.dart';
 
 class SalesProvider {
   Future<List<SalesItem>> getSalesItems() async{
+    String POSProfile=await FrappeGet.value(docType: 'POS Profile');
     var queryParam = {
       "start": 0,
       "page_length": 40,
-      "price_list": await FrappeGet.value('Price List'),
-      "item_group": "All Item Groups", //await FrappeGet.value('Item Group'),
+      "price_list": await FrappeGet.value(docType: 'POS Profile',fieldName: 'selling_price_list',filters: null ),
+      "item_group": await FrappeGet.value(docType: 'Item Group',filters: {'is_group':1}),
       "search_term": "",
-      "pos_profile": await FrappeGet.value('POS Profile')
+      "pos_profile": POSProfile
     };
     Response? response = await DioClient().get('/method/erpnext.selling.page.point_of_sale.point_of_sale.get_items',queryParameters: queryParam);
     return List.from(response?.data['message']['items']).map((item) => SalesItem.fromJson(item)).toList();
+  }
+  
+   saveSales(SalesInvoice salesInvoice) async{
+
+   Response? response = await DioClient().post('/resource/Sales Invoice',data:salesInvoice.toJson());
   }
 }
